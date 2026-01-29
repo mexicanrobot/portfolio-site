5ad30fb65fa8d714262461d76b89648206538d78
Alex <alex@alexs-air.lan>
2026-01-28 19:09:02 -0800

    Change homepage, edit social icons

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Alejandro Abaroa";
+export const SITE_DESCRIPTION = "Portfolio of Alejandro Abaroa, a software engineer with a passion for design, UI/UX, and building products that people love.";
